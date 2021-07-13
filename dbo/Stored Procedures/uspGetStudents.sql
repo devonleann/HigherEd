11f057d7c2269452fd75e72faeef1c4c5cc13eb8
Devon Wilson <devonleann@gmail.com>
2021-07-13 02:53:18 -0500

    Added fully qualified column name in usp

--- dbo/Stored Procedures/uspGetStudents.sql
+++ dbo/Stored Procedures/uspGetStudents.sql
@@ -14,6 +14,6 @@ BEGIN
      ON Student.StudentID = FinancialAid.StudentID 
      ON StudentContact.ContactID = Student.ContactID
   WHERE Student.StudentID IS NOT NULL
-    AND StudentID <> 1880
+    AND student.StudentID <> 1880
 
 END
